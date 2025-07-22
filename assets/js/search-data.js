b8cb960e9d276c8e946e8ddba46656c4a7d9a829
MohammedBarakat7 <153748272+MohammedBarakat7@users.noreply.github.com>
2025-07-22 05:43:04 +0000

    Deploying to gh-pages from @ MohammedBarakat7/MohammedBarakat7.github.io@9e438d8a034e983b4bab655d366f5cad3c5d632b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6F%68%61%6D%6D%65%64%64%62%61%72%61%6B%61%74%37@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +509,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mohammedbarakat05", "_blank");
         },
       },{
         id: 'social-rss',
